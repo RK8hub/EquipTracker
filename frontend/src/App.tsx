38447230a9a00f2774ba92c062c5daebb707e948
RK8hub <RK8DEV@gmail.com>
2026-07-24 10:00:40 -0600

    feat: add Axios client with 401 interceptor and Toaster

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div className="p-8 text-center text-lg font-semibold">EquipTracker</div>} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<div className="p-8 text-center text-lg font-semibold">EquipTracker</div>} />
+      </Routes>
+      <Toaster richColors closeButton />
+    </>
   )
 }
 
